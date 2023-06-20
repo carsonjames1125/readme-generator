e4c29a26731a4f7a611e4fe964189981c21b4fd8
carsonjames1125 <tarheelkid44@yahoo.com>
2023-06-20 09:41:25 -0400

    began developing questions and pseudocode for the readme generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//need to give the user options to for the different sections of the readme and then add them to the table of contents section
+
+const questions = [{
+    type: ' input',
+    name: 'title',
+    message: 'What is the name of your repository?'
+    //needs to be included
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please enter a valid title.')
+            return false;
+        }
+    }
+},
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
