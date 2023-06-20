cc8451ee7ed12bf40cc8a2c7e10fcc5010e2e1db
carsonjames1125 <tarheelkid44@yahoo.com>
2023-06-20 10:06:23 -0400

    added more confirm and input sections for the readme application

--- Develop/index.js
+++ Develop/index.js
@@ -10,18 +10,93 @@ const util = require('util');
 const questions = [{
     type: ' input',
     name: 'title',
-    message: 'What is the name of your repository?'
+    message: 'What is the name of your repository?',
     //needs to be included
-    validate: titleInput => {
-        if (titleInput) {
+    validate: nameInput => {
+        if (nameInput) {
             return true;
         } else {
-            console.log('Please enter a valid title.')
+            console.log('Please enter a valid title.');
             return false;
         }
     }
 },
-];
+{ // description section
+    type: 'input',
+    name: 'description',
+    message: 'How would you descripe this repository? Please answer in a few short sentences.',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter a description.');
+            return false;
+        }
+    }
+},
+
+{ //need to know if there is an installation process this section allows the user to determine yes or no.
+
+    type: 'confirm',
+    name: 'confirmInstallation',
+    message: 'Does your repository contain an installation section?',
+   
+},
+{ // installation section
+    type: 'input',
+    name: 'installation',
+    message: 'What are your installation instructions?',
+    // the when statement allows the previous section to take in the user input and if selected yes the user it prompted with the area to enter the installation instructions, if no the section is skipped.
+    when: ({ confirmInstallation }) => {
+        if (confirmInstallation) {
+            return true;
+        } else {
+            return false;
+        }
+}},
+{ //usage section
+    type: 'confirm',
+    name: 'confirmUsage',
+    message: 'Would you like to add any specific instructions on how to use the application?',
+},
+{
+    type: 'input',
+    name: 'usageInstructions',
+    message: 'Please list any instructions here. Add any images or links to help explain.',
+    when: ({confirmUsage}) => {
+        if (confirmUsage) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+{ // contribution section
+
+    type: 'confirm',
+    name: 'confirmContribution',
+    message: 'Were any other developers that contributed to the project?',
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Please list and explain contributers.',
+    when: ({confirmContribution}) => {
+        if (confirmContribution) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+{ // testing section
+    type: 'confirm',
+    name: 'testConfirm',
+    message: 'Is there testing available within this application?',
+},
+{
+    type:
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
